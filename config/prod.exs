f50b22e78ebb137ef712322e2d6f32093478e6cd
Rockwell Schrock <schrockwell@gmail.com>
2020-06-21 11:13:44 -0400

    Adding Dokku config

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,11 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :lumber, LumberWeb.Endpoint,
-  url: [host: "logs.wwsac.com", port: 443, scheme: "https"],
+  url: [
+    host: System.get_env("WEB_HOST") || "logs.wwsac.com",
+    port: 443,
+    scheme: "https"
+  ],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
