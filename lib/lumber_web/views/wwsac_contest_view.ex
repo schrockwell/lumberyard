a01d0e1c6cd84cdf0d9c301414a5d69f1029974e
Rockwell Schrock <schrockwell@gmail.com>
2020-06-23 00:47:24 -0400

    Don't show empty categories on contest results and leaderboards

--- lib/lumber_web/views/wwsac_contest_view.ex
+++ lib/lumber_web/views/wwsac_contest_view.ex
@@ -1,3 +1,11 @@
 defmodule LumberWeb.WwsacContestView do
   use LumberWeb, :view
+
+  defp empty_categories(results) do
+    Enum.filter(results, fn r -> r.operators == [] end)
+  end
+
+  defp all_empty?(results) do
+    Enum.all?(results, fn r -> r.operators == [] end)
+  end
 end
--- lib/lumber_web/views/wwsac_leaderboard_view.ex
+++ lib/lumber_web/views/wwsac_leaderboard_view.ex
@@ -1,3 +1,11 @@
 defmodule LumberWeb.WwsacLeaderboardView do
   use LumberWeb, :view
+
+  defp empty_categories(leaderboards) do
+    Enum.filter(leaderboards, fn lb -> lb.operators == [] end)
+  end
+
+  defp all_empty?(leaderboards) do
+    Enum.all?(leaderboards, fn lb -> lb.operators == [] end)
+  end
 end
