70750a85cf79a058e0b368b4a44339f7e15dc310
Rockwell Schrock <schrockwell@gmail.com>
2020-08-05 09:03:33 -0400

    Cleanup warnings

--- lib/lumber_web/views/wwsac_contest_view.ex
+++ lib/lumber_web/views/wwsac_contest_view.ex
@@ -1,10 +1,6 @@
 defmodule LumberWeb.WwsacContestView do
   use LumberWeb, :view
 
-  defp empty_categories(results) do
-    Enum.filter(results, fn r -> r.operators == [] end)
-  end
-
   defp all_empty?(results) do
     Enum.all?(results, fn r -> r.operators == [] end)
   end
--- lib/lumber_web/views/wwsac_leaderboard_view.ex
+++ lib/lumber_web/views/wwsac_leaderboard_view.ex
@@ -1,10 +1,6 @@
 defmodule LumberWeb.WwsacLeaderboardView do
   use LumberWeb, :view
 
-  defp empty_categories(leaderboards) do
-    Enum.filter(leaderboards, fn lb -> lb.operators == [] end)
-  end
-
   defp all_empty?(leaderboards) do
     Enum.all?(leaderboards, fn lb -> lb.operators == [] end)
   end
