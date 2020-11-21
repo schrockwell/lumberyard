453fce63e0a0b5076d8533264a5341ee2ffa2087
Rockwell <schrockwell@gmail.com>
2020-11-21 01:29:18 +0000

    Moving dev database config to $DATABASE_URL

--- config/dev.exs
+++ config/dev.exs
@@ -1,13 +1,13 @@
 use Mix.Config
 
 # Configure your database
+# DATABASE_URL is read in the Lumber.Repo.init/2 callback
 config :lumber, Lumber.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "lumber_dev",
-  hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
-  pool_size: 10
+  pool_size: 2,
+  maintenance_database: "defaultdb",
+  ssl: true
+
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
--- lib/lumber/repo.ex
+++ lib/lumber/repo.ex
@@ -2,4 +2,8 @@ defmodule Lumber.Repo do
   use Ecto.Repo,
     otp_app: :lumber,
     adapter: Ecto.Adapters.Postgres
+
+    def init(_type, config) do
+      {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+    end
 end
